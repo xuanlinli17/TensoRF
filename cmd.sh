d8a9d6922d3e1b1941626386c8d6a0ae1ecbe192
Xinyue Wei <13659185706@163.com>
2023-02-03 18:31:34 -0800

    change config

---
+++ cmd.sh
@@ -0,0 +1,9 @@
+python train.py --config configs/lego.txt --datadir /home/sarahwei/dataset/nerf_synthetic/chair --expname tensorf_chair_VM
+python train.py --config configs/lego.txt --datadir /home/sarahwei/dataset/nerf_synthetic/drums --expname tensorf_drums_VM
+python train.py --config configs/lego.txt --datadir /home/sarahwei/dataset/nerf_synthetic/ficus --expname tensorf_ficus_VM
+python train.py --config configs/lego.txt --datadir /home/sarahwei/dataset/nerf_synthetic/hotdog --expname tensorf_hotdog_VM
+python train.py --config configs/lego.txt --datadir /home/sarahwei/dataset/nerf_synthetic/materials --expname tensorf_materials_VM
+python train.py --config configs/lego.txt --datadir /home/sarahwei/dataset/nerf_synthetic/mic --expname tensorf_mic_VM
+python train.py --config configs/lego.txt --datadir /home/sarahwei/dataset/nerf_synthetic/ship --expname tensorf_ship_VM
+
+python train.py --config configs/lego.txt --datadir /home/sarahwei/dataset/nerf_synthetic/lego --expname test
